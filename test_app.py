63ac98839e9ec8ba9952a2543264f6281330703a
aman <amangoswami@gmail.com>
2024-05-23 21:20:21 +0530

    new for pipeline

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = quote('heyaman how are you')
+    expected_text = quote('heyaman how are you??')
     assert expected_text.encode() in response.data
