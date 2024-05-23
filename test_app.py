129ba5074a59b61dd4b49f420d34b63ae09d39c7
aman <amangoswami@gmail.com>
2024-05-23 20:42:37 +0530

    trigger

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = quote('hey gfg')
+    expected_text = quote('hey gfg2001')
     assert expected_text.encode() in response.data
